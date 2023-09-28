4c51176ae8c516202a31857a0d46b705ab41e7fd
sensreeabhi <sensreeabhi@gmail.com>
2023-09-28 19:00:24 -0400

    First Commit

---
+++ ExampleGit/src/exampleGit/SamplePro.java
@@ -0,0 +1,13 @@
+package exampleGit;
+
+public class SamplePro {
+
+	public static void main(String[] args) {
+		int a= 12 ,b =30;
+		if(a>b)
+			System.out.println("a is Bigger");
+		else 
+			System.out.println("b is Bigger");
+	}
+
+}
